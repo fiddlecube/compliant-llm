5d3456558545f0ce667d566bca111cee6af7a7a0
Neha Nupoor <nupoor.neha@gmail.com>
2025-05-14 21:22:19 -0700

    fixed import err

--- core/strategies/__init__.py
+++ core/strategies/__init__.py
@@ -1,12 +1,16 @@
+# flake8: noqa E501
 """
 Attack strategies package.
 
 This package contains implementations of various attack strategies for testing LLMs.
 """
 from .base import BaseAttackStrategy
-from .attack_enhancers.enhanced_strategy import EnhancedAttackStrategy
+from ._attack_enhancers._enhanced_strategy import EnhancedAttackStrategy
+from .attack_strategies.strategy import JailbreakStrategy, PromptInjectionStrategy
 
 __all__ = [
     'BaseAttackStrategy',
     'EnhancedAttackStrategy',
+    'JailbreakStrategy',
+    'PromptInjectionStrategy',
 ]
