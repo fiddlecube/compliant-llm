d2c11716116ee9acb4aac9db445dc703c091ada1
Vini Katyal <vinikatyal90@gmail.com>
2025-05-28 21:11:35 +0530

    chore: hard coded values

--- core/analytics/settings.py
+++ core/analytics/settings.py
@@ -1,10 +1,8 @@
-import os
-
 def get_azure_settings():
-    """Get Azure Monitor settings from environment variables."""
+    """Get Azure Monitor settings."""
     return {
-        "instrumentation_key": os.getenv("AZURE_INSTRUMENTATION_KEY", "ed532436-db1f-46bb-aeef-17cb4f3dcf8b"),
-        "ingestion_endpoint": os.getenv("AZURE_INGESTION_ENDPOINT", "https://westus-0.in.applicationinsights.azure.com/"),
+        "instrumentation_key": "ed532436-db1f-46bb-aeef-17cb4f3dcf8b",
+        "ingestion_endpoint": "https://westus-0.in.applicationinsights.azure.com/",
     }
 
 azure_settings = get_azure_settings()
